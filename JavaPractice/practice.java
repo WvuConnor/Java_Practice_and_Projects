3976c8b0ca5333dc2e59783f9dac129389f5f8fe
WvuConnor <caw00006@mix.wvu.edu>
2026-02-16 06:55:16 -0500

    Starting point

---
+++ JavaPractice/practice.java
@@ -0,0 +1,9 @@
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    //TODO geeks 4 geeks numbers 4-10 1hr
+    //TODO create indvidual clasess for each of them
+    
+    //TODO Study for interview 1hr
+}
